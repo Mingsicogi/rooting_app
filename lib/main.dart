6d10e6d063b45a579912b8807040de2a17c93134
전민석 <minssogi@line.games>
2020-07-17 02:28:57 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:root_checker/root_checker.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  // 루팅 디바이스인지 체크
+  bool _isDeviceRooted = false;
+  void checkRootingDevice() async {
+    bool isDeviceRooted = await RootChecker.isDeviceRooted;
+
+    setState(() {
+      _isDeviceRooted = isDeviceRooted;
+    });
+  }
+
+  @override
+  void initState() {
+    checkRootingDevice(); //최초 초기화 과정에서 루팅 디바이스 체크 메소드 호출
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if('$_isDeviceRooted' == 'true') {
+      SystemNavigator.pop(); // app 종료
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
